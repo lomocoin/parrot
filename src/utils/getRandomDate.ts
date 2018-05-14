1bd7dcc2bac9a32edab2054e655c695bb8e07af9
ZhangRGK <stamen0630@gmail.com>
2018-05-14 23:39:58 +0800

    FEAT(decorators): add new column type: date

---
+++ src/utils/getRandomDate.ts
@@ -0,0 +1,8 @@
+import moment from 'moment';
+import getRandomNumber from './getRandomNumber';
+
+export default (format: string, start?: string, end?:string): moment.Moment => {
+  const endDate = end ? moment(end, format).valueOf() : moment().valueOf();
+  const startDate = start ? moment(start, format).valueOf() : 0;
+  return moment(getRandomNumber({ limit: [startDate, endDate] }));
+}
