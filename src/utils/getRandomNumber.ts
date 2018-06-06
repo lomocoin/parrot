b7c6b1a22c2d761788c8e54b69bb074debf0a880
ZhangRGK <stamen0630@gmail.com>
2018-06-06 17:58:42 +0800

    FEAT(test): add date test, fix getRandomNumber bug

--- src/utils/getRandomNumber.ts
+++ src/utils/getRandomNumber.ts
@@ -3,5 +3,5 @@ export default ({ limit }: { limit: [number, number] | number }): number => {
     limit = [0, limit];
   }
   const [low, high] = limit;
-  return Math.ceil(Math.random() * (high - low) + low);
+  return Number.parseInt((Math.random() * (high - low) + low).toFixed());
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,7 +1,9 @@
+import moment from 'moment';
 import getRandomString from '../src/utils/getRandomString';
 import getRandomNumber from '../src/utils/getRandomNumber';
 import getRandomBoolean from '../src/utils/getRandomBoolean';
 import getRandom from '../src/utils/getRandom';
+import getRandomDate from '../src/utils/getRandomDate';
 import applyMixins from '../src/utils/applyMixins';
 import splitPath from '../src/utils/splitPath';
 
@@ -27,9 +29,9 @@ describe('getRandomString', () => {
     expect(str.length).toBeGreaterThanOrEqual(5);
   });
   test('should get a random string length between 0 to limit', () => {
-    const str = getRandomString({ limit: 20 });
+    const str = getRandomString({ limit: 200 });
     expect(str).toMatch(/\w*/)
-    expect(str.length).toBeLessThanOrEqual(20)
+    expect(str.length).toBeLessThanOrEqual(200)
     expect(str.length).toBeGreaterThanOrEqual(0);
   });
 });
@@ -58,7 +60,26 @@ describe('getRandom', () => {
     }];
     const player = getRandom(objectSouce);
     expect(objectSouce).toContain(player);
-  })
+  });
+});
+
+describe('getRandomDate', () => {
+  test('should get a moment instance of random date', () => {
+    const m = getRandomDate();
+    expect(moment.isMoment(m)).toBe(true);
+  });
+  test('should get a moment instance between startTime and endTime', () => {
+    const m = getRandomDate('YYYY-MM-DD', '2018-05-01', '2018-05-31');
+    expect(m.isBetween('2018-04-30', '2018-06-01')).toBe(true);
+  });
+  test('should get random date between 1970-01-01 and endTime if startTime is null', () => {
+    const m = getRandomDate('YYYY-MM-DD', null, '2018-05-31');
+    expect(m.isBetween('1970-01-01', '2018-05-31')).toBe(true);
+  });
+  test('should get random date between startTime and now if endTime is null', () => {
+    const m = getRandomDate('YYYY-MM-DD', '2018-05-01');
+    expect(m.isBetween('1970-05-01', moment())).toBe(true);
+  });
 });
 
 describe('applyMixins', () => {
@@ -84,7 +105,7 @@ describe('applyMixins', () => {
 
     const monster = new Monster();
 
-    expect(monster.woof()).toBe('Woo;f!');
+    expect(monster.woof()).toBe('Woof!');
     expect(monster.fly()).toBe('Flying');
   })
 });
