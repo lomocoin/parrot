d7c45e799880bb1ba3294c3ec4957414e0201621
Thomas Gazzoni <thomas.gazzoni@gmail.com>
2018-07-02 11:35:15 +0800

    ADD(route): checkToken url FIX(mock): typings REMOVE(utils): image type

--- src/index.ts
+++ src/index.ts
@@ -17,38 +17,50 @@ export class Server {
   config: any;
   tokens: Set<string> = new Set<string>();
 
-  constructor(option: { config: string, quite: boolean }) {
-
+  constructor(option: { config: string; quite: boolean }) {
     const config: any = {
       port: 7001,
       outDir: ['./.mockCache'],
-      quite: false,
+      quite: false
     };
     if (fs.existsSync(resolve('.', option.config))) {
-      Object.assign(config, JSON.parse(fs.readFileSync(resolve('.', option.config), 'utf-8')));
+      Object.assign(
+        config,
+        JSON.parse(fs.readFileSync(resolve('.', option.config), 'utf-8'))
+      );
     }
-    this.config = { ...config, ...{
-      outDir: config.compilerOptions.outDir || config.include,
-      imagePath: config.imagePath || './.mockCache/imgs',
-    }};
+    this.config = {
+      ...config,
+      ...{
+        outDir: config.compilerOptions.outDir || config.include,
+        imagePath: config.imagePath || './.mockCache/imgs'
+      }
+    };
     this.app = express();
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(cookieParser());
+    this.app.get('/checkToken', (req: Request, res: Response) => {
+      const token = req.get('authorization');
+      if (!token || !this.tokens.has(token)) {
+        return res.status(401).end();
+      }
+      return res.status(200).json({ token });
+    });
     this.app.post('/signIn', (req: Request, res: Response) => {
       const { username, password } = req.body;
-
+      
       if (
-        !username
-        || !password
-        || username !== this.config.auth.username
-        || password !== this.config.auth.password
+        !username ||
+        !password ||
+        username !== this.config.auth.username ||
+        password !== this.config.auth.password
       ) {
         return res.status(422).end();
       }
       const token = uuidV4();
       this.tokens.add(token);
-      return res.status(200).json(token);
+      return res.status(200).json({ token });
     });
     this.app.post('/signOut', (req: Request, res: Response) => {
       const token = req.get('authorization');
@@ -57,9 +69,12 @@ export class Server {
       }
       this.tokens.delete(token);
       return res.status(200).end();
-    })
+    });
 
-    this.app.use('/static', express.static(resolve('.', this.config.imagePath)))
+    this.app.use(
+      '/static',
+      express.static(resolve('.', this.config.imagePath))
+    );
     this.app.use((req: Request, res: Response, next: NextFunction) => {
       if ([...this.config.auth.whiteList, '/signIn'].indexOf(req.url) >= 0) {
         return next();
@@ -70,16 +85,18 @@ export class Server {
         return res.status(401).end();
       }
       next();
-    })
+    });
     this.app.use(mockMiddleware(this.config));
 
-    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      let status = 500;
-      if (!Number.isNaN(Number.parseInt(err.message))) {
-        status = Number.parseInt(err.message, 10);
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        let status = 500;
+        if (!Number.isNaN(Number.parseInt(err.message))) {
+          status = Number.parseInt(err.message, 10);
+        }
+        res.status(status).end();
       }
-      res.status(status).end();
-    });
+    );
 
     this.app.set('port', this.config.port);
   }
